3aa36547344c4bcccbdd811e0dda6ef69ebd92e8
niin12 <148969143+niin12@users.noreply.github.com>
2023-11-14 23:32:18 -0600

    Add files via upload

---
+++ login.js
@@ -0,0 +1,26 @@
+const form = document.querySelector("form")
+
+form.addEventListener("submit",(e)=>{
+    e.preventDefault()
+
+    const username = form.username.value
+    const password = form.password.value
+
+    const authenticated = authentication(username,password)
+
+    if(authenticated){
+        window.location.href = "niin1.html"
+    }else{
+        alert("you inported a wrong Key or password")
+    }
+})
+
+// function for checking username and password
+
+function authentication(username,password){
+    if(username === "niin1-1" && password === "admin"){
+        return true
+    }else{
+        return false
+    }
+}
