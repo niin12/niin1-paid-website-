5ec176325f555e3a82f144d9e7a2cb6043f6991d
niin12 <148969143+niin12@users.noreply.github.com>
2023-11-15 13:52:36 -0600

    Update login.js

--- login.js
+++ login.js
@@ -18,7 +18,7 @@ form.addEventListener("submit",(e)=>{
 // function for checking username and password
 
 function authentication(username,password){
-    if(username === "niin1-123215689" && password === "bypass"){
+    if(username === "niin1-1222-32214" && password === "bypass"){
         return true
     }else{
         return false
